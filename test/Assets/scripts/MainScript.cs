4d2f4a00e0f84f13cd7222ee6a82e1eb55f25604
DealCore <35450852+DealCore@users.noreply.github.com>
2018-02-15 18:56:21 +0200

    UnityProjectRobot
    Added Unity project

---
+++ test/Assets/scripts/MainScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MainScript : MonoBehaviour {
+	
+	Animator anim;
+
+	void Start()
+	{
+		anim = GetComponent<Animator> ();
+
+	}
+
+	void Update()
+	{
+		
+		if (Input.touchCount > 0) {
+			anim.Play ("Emergence");
+		}
+	
+	}
+}
---
+++ test/Assets/shade/Simon.shader
@@ -0,0 +1,12 @@
+﻿Shader "Custom/Simon" {
+	Properties {
+		_MainTex("Texture", 2D) = "White" {}
+	}
+	SubShader {
+		Pass
+		{
+			
+			SetTexture[_MainTex]
+		}
+	}
+}
